99256022a70a9b820dcff33a6fb4cc5936a260de
Muhammad Awais Awan <awaixsawan@gmail.com>
2022-07-19 14:19:01 +0500

    project completed

--- src/Components/GoalHeader.js
+++ src/Components/GoalHeader.js
@@ -2,20 +2,30 @@ import React, { useState } from "react";
 import "./GoalHeader.css";
 export default function GoalHeader(props) {
   const [input, setInput] = useState("");
-
+  const [isNotEmpty, setisNotEmpty] = useState(true);
   const buttonSubmitHandler = () => {
     const goalitem = { label: input };
-
+    if (input.trim().length === 0) {
+      setisNotEmpty(false);
+      return;
+    }
     props.onAddNewGoal(goalitem);
   };
   const inputChangeHandler = (event) => {
     setInput(event.target.value);
+    setisNotEmpty(true);
   };
 
   return (
     <div className="header">
-      <h3 className="goal-header__label">Course Goal</h3>
+      <h3 className={`goal-header__label ${isNotEmpty ? "" : "invalid"}`}>
+        Course Goal
+      </h3>
       <input
+        style={{
+          borderColor: isNotEmpty ? "black" : "red",
+          background: isNotEmpty ? "transparent" : "salmon",
+        }}
         className="goal-header__input"
         type="text"
         value={input}
